4d7c85e93da7a69f75c9cfe93fb3ff9d210adb88
Thafne <107944923+Thafne@users.noreply.github.com>
2022-07-31 11:32:21 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,18 @@
-## The makeCacheMix consists of set, get, setinv and getinv
+# The makeCacheMix consists of set, get, setinv and getinv
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL  ##initializing inverse as NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL  
   }
-  get <- function() x         ## function to get matrix x
+  get <- function() x         # function to get matrix x
   setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv         ##function to obtain inverse of the matrix 
+  getinv <- function() inv         #function to obtain inverse of the matrix 
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
-##The cacheSolve is used to get the cache data
+#The cacheSolve is used to get the cache data
 cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
+# Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
   if(!is.null(inv)) {
     message("getting cached result")
@@ -21,10 +21,10 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   inv <- solve(data, ...)  ## This calculete inverse value
   x$setinv(inv)
-  inv       ##Return a matrix that is the inverse of 'x'
+  inv       #Return a matrix that is the inverse of 'x'
 }
 
-## For chechinkg the program:
+# For chechinkg the program:
 m <- matrix(rnorm(16),4,4)
 m1 <- makeCacheMatrix(m)
 cacheSolve(m1)
